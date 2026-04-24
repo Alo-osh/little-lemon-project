041d94c856c3f61c0fc250c0accbfb91e4e3c071
Alo-osh <habeebali072@gmail.com>
2026-04-24 23:52:42 +0300

    modefied the test

--- src/App.test.js
+++ src/App.test.js
@@ -4,18 +4,13 @@ import LogIn from './Components/LogIn.js'
 test("removes reserved time correctly", () => {
   const handleSubmit=jest.fn();
   render(<LogIn />)
-  const email=screen.getByLabelText(/email:/i);
-  fireEvent.change(email,{target:{value:"habeebali072@gmail.com"}});
-  const passWord=screen.getByLabelText(/password:/i);
-  fireEvent.change(passWord,{target:{value:"12345678"}})
-  const submitButton=screen.getByRole("button");
-  fireEvent.click(submitButton)
     const email2=screen.getByLabelText(/email:/i);
   fireEvent.change(email2,{target:{value:"habeebali072@gmail.com"}});
   const passWord2=screen.getByLabelText(/password:/i);
   fireEvent.change(passWord2,{target:{value:"125678"}})
   const submitButton2=screen.getByRole("button");
 fireEvent.click(submitButton2)
+  expect(submitButton2).toBeDisabled();
   const initialState = {
     e: ["17:00", "18:00", "19:00"],
   };
@@ -26,8 +21,6 @@ fireEvent.click(submitButton2)
   };
 
   const result = updateTimes(initialState, action);
-  expect(submitButton).not.toHaveAttribute("disabled");
-  expect(submitButton2).toHaveAttribute("disabled");
 
   expect(result.e).toEqual(["18:00", "19:00"]);
 });
